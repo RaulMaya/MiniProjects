9c272cecf96f9d55d74f63cd5a5cfbf776dff2a8
Raul <raulmayas20@gmail.com>
2022-10-06 16:37:59 -0500

    Init Team Slider Mini Project

---
+++ team-slider/assets/scripts/apps.js
@@ -0,0 +1,50 @@
+const frontBtn = document.getElementById('front');
+const backBtn = document.getElementById('back');
+const myList= document.querySelector('ul');
+let listItem = myList.firstElementChild
+const listLength = myList.querySelectorAll('li').length
+let i = 0;
+
+const listItems = myList.querySelectorAll('li')
+const arrayList = Array.from(listItems)
+
+console.log(arrayList);
+console.log(arrayList[2]);
+console.log(arrayList.indexOf(listItem));
+
+function start() {
+    listItem.classList.toggle('visible');
+}
+
+function nextSlide() {
+    const index = arrayList.indexOf(listItem)
+    if (index < listLength-1) {
+        listItem.classList.toggle('visible');
+        listItem.nextElementSibling.classList.toggle('visible');
+        listItem = listItem.nextElementSibling;
+    } else {
+        listItem.classList.toggle('visible');
+        listItem = myList.firstElementChild;
+        listItem.classList.toggle('visible');
+    }
+
+}
+
+function backSlide() {
+    const index = arrayList.indexOf(listItem)
+    if (index === 0) {
+        listItem.classList.toggle('visible');
+        listItem = myList.lastElementChild;
+        listItem.classList.toggle('visible');
+    } else {
+        listItem.classList.toggle('visible');
+        listItem.previousElementSibling.classList.toggle('visible');
+        listItem = listItem.previousElementSibling;
+    }
+
+}
+
+start();
+
+frontBtn.addEventListener('click', nextSlide)
+backBtn.addEventListener('click', backSlide)
