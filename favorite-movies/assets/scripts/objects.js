1dce9c63348d6393957ca49b12d32f3726d2b658
Raul <raulmayas20@gmail.com>
2022-10-18 09:05:08 -0500

    Favorite Movies JS

--- favorite-movies/assets/scripts/objects.js
+++ favorite-movies/assets/scripts/objects.js
@@ -1,51 +1,14 @@
-let person = {
-    name: 'Raul',
-    age: 26,
-    hobbies: ['Sports', 'TV Series'],
-    greet: function() {
-        // alert('I want to become a Front-End Developer');
-        console.log('I want to become a Front-End Developer');
-    }
-};
+const sections = document.getElementsByTagName('section');
 
-let personTwo = {
-    'first-name': 'Willy',
-    'dog age': 10,
-    hobbies: ['Sleep', 'Bark'],
-    greet: function() {
-        // alert('I want to become a Front-End Developer');
-        console.log('Im a dog');
-    },
-    1.5: 'I like to be with Raul'
-};
+const addSection = sections[0];
+const filterSection = sections[1];
+const myList = filterSection.nextElementSibling
+console.log(myList)
 
-// const movieList = document.getElementById('movie-list');
+const addMovieBtn = addSection.querySelector('button');
+const favMovieInput = addSection.querySelectorAll('input')[0];
+const extraName = addSection.querySelectorAll('input')[1];
+const extraValue = addSection.querySelectorAll('input')[2];
 
-// movieList.style['background-color'] = 'red';
-// movieList.style.display = 'block';
-
-console.log(personTwo['first-name']);
-console.log(personTwo['dog age']);
-console.log(personTwo[1.5]);
-
-
-// Don't Do
-/* person = {
-    name: 'Raul',
-    age: 26,
-    hobbies: ['Sports', 'TV Series'],
-    greet: function() {
-        // alert('I want to become a Front-End Developer');
-        console.log('I want to become a Front-End Developer');
-    },
-    isAdmin: true 
-}; */
-
-// Do
-person.isAdmin = true; // Reasign a property 
-
-delete person.age; // Deleting a property (person.age = undefined), We use null to reset a value
-// person.age = null;
-
-console.log(person.isAdmin)
-console.log(person)
+const searchBtn = filterSection.querySelector('button');
+const filterInput = filterSection.querySelector('input');
